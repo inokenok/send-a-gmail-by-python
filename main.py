8ec3bfabd485215891e23bf19f750371f751ca85
inokenok <setsu5579@gmail.com>
2024-10-22 17:32:13 +0900

    send a mail some people.py

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+# in ierminal
 from mail import send_mail
-to = input("送信先")
-subject = input("題")
-body = input("内容")
-
-send_mail(to, subject, body)
+import time
+n = int(input("Number of people："))
+tol = []
+bodyl = []
+subjectl = []
+for i in range(n):
+    tol.append(input(f"person you want to send{i + 1}："))
+    bodyl.append(input(f"message you want to send {i + 1} person："))
+    subjectl.append(input(f"{i + 1}person title："))
+for i in range(n):
+    to = tol[i]
+    body = bodyl[i]
+    subject = subjectl[i]
+    send_mail(to, body, subject)
+    print(f"{i+1}person done")
+    time.sleep(5)
+print("All Succes")
